15e2b88a8a141780516abe4193365f446ca7c7a9
zinjkov <zinjkov.su@gmail.com>
2018-04-19 00:26:03 +1000

    added conditional compilation

--- step_motor.cpp
+++ step_motor.cpp
@@ -1,7 +1,11 @@
 #include "step_motor.h"
 #include <Arduino.h>
-//#include <SoftPWM.h>
 
+//#define PWMCONTROL
+
+#ifdef PWMCONTROL
+#include <SoftPWM.h>
+#endif
 step_motor::step_motor()
 {
 }
@@ -18,31 +22,44 @@ void step_motor::set(int8_t pin_l, int8_t pin_r)
 
 void step_motor::init()
 {
-	//SoftPWMBegin();
-	//SoftPWMSet(m_pin_left, 0);
-	//SoftPWMSet(m_pin_right, 0);
-	
+#ifdef PWMCONTROL
+	SoftPWMBegin();
+	SoftPWMSet(m_pin_left, 0);
+	SoftPWMSet(m_pin_right, 0);
+#else
+	pinMode(m_pin_left, OUTPUT);
+	pinMode(m_pin_right, OUTPUT);
+#endif
 }
 
 
 void step_motor::write(int8_t power)
 {
 	if (power > 0) {
-		//SoftPWMSet(m_pin_left, 0);
-		//SoftPWMSet(m_pin_right, 0);
-	//	digitalWrite(m_pin_left, HIGH);
-	//	digitalWrite(m_pin_right, LOW);
+#ifdef PWMCONTROL
+		SoftPWMSet(m_pin_left, 0);
+		SoftPWMSet(m_pin_right, 0);
+#else
+		digitalWrite(m_pin_left, HIGH);
+		digitalWrite(m_pin_right, LOW);
+#endif
 	} else if (power < 0) {
-		//SoftPWMSet(m_pin_left, 0);
-		//SoftPWMSet(m_pin_right, 150);
-		//digitalWrite(m_pin_left, LOW);
-		//digitalWrite(m_pin_right, HIGH);
+#ifdef PWMCONTROL
+		SoftPWMSet(m_pin_left, 0);
+		SoftPWMSet(m_pin_right, 150);
+#else
+		digitalWrite(m_pin_left, LOW);
+		digitalWrite(m_pin_right, HIGH);
+#endif
 	}
 	else {
-//		SoftPWMSet(m_pin_left, 150);
-	//	SoftPWMSet(m_pin_right, 0);
-		//digitalWrite(m_pin_left, LOW);
-		///digitalWrite(m_pin_right, LOW);
+#ifdef PWMCONTROL
+		SoftPWMSet(m_pin_left, 150);
+		SoftPWMSet(m_pin_right, 0);
+#else
+		digitalWrite(m_pin_left, LOW);
+		digitalWrite(m_pin_right, LOW);
+#endif
 	}
 }
 
