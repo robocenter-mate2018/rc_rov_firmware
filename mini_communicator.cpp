0e2dee8840be14724333629e6cfd7ddd557f491b
zinjkov <zinjkov.su@gmail.com>
2018-04-30 03:57:02 +1000

    added timeout for sending data to mini

--- mini_communicator.cpp
+++ mini_communicator.cpp
@@ -15,26 +15,36 @@ mini_communicator::~mini_communicator()
 void mini_communicator::init()
 {
 	mini_payload.begin(115200);
+	m_timer.start();
 }
 
 void mini_communicator::run(const data_store & store)
 {
-	uint8_t buffer[50];
-	rov_types::rov_hardware_control control = store.get_control();
-	rov_types::rov_mini_control t;
-	t.acoustics = control.acoustic;
-	for (int i = 0; i < 4; i++) {
-		t.twisting_motors[i] = control.twisting_motors[i];
+	if (m_timer.elapsed() > 15) {
+
+		uint8_t buffer[50] = {0};
+		rov_types::rov_hardware_control control = store.get_control();
+		rov_types::rov_mini_control t;
+
+		t.acoustics = control.acoustic;
+		t.magnet = control.magnet;
+
+		for (int i = 0; i < 4; i++) {
+			t.twisting_motors[i] = control.twisting_motors[i];
+		}
+
+		uint8_t size = t.serialize(buffer);
+		mini_payload.write(buffer, size);
+		m_timer.restart();
 	}
-	t.magnet = control.magnet;
-	uint8_t size = t.serialize(buffer);
-	mini_payload.write(buffer, size);
 }
 
 void mini_communicator::commit(data_store & store)
 {
 	store.get_telimetry().acoustic = this->m_last_telimetry.acoustics;
 	store.get_telimetry().magnet = this->m_last_telimetry.magnet;
+	store.get_telimetry().mini_communication = this->m_last_telimetry.mini_communicator_feedback;
+	
 	for (int i = 0; i < 4; i++) {
 		store.get_telimetry().twisting_motors[i] = this->m_last_telimetry.magnet;
 	}
@@ -54,7 +64,9 @@ void mini_communicator::on_serial_event()
 	while (mini_payload.available()) {
 		packet[i++] = mini_payload.read();
 	}
+
 	auto e = hc.deserialize(packet, i);
+
 	if (rov_types::serializable::check_for_success(e)) {
 		m_last_telimetry = hc;
 		m_updated = true;
