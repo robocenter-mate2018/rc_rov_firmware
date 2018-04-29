3660fcea5e7e756e81129502990369be783e14e0
zinjkov <zinjkov.su@gmail.com>
2018-04-30 03:59:43 +1000

    set actual direction

--- thruster_subsystem.cpp
+++ thruster_subsystem.cpp
@@ -1,20 +1,19 @@
 #include "thruster_subsystem.h"
 
 
-
 thruster_subsystem::thruster_subsystem()
 {
 	int i = 0;
-	m_motors[i++].set(config::thrusters::horizontal::pins::FRONT_LEFT_PIN);
-	m_motors[i++].set(config::thrusters::horizontal::pins::FRONT_RIGHT_PIN);
-	m_motors[i++].set(config::thrusters::horizontal::pins::BACK_LEFT_PIN);
-	m_motors[i++].set(config::thrusters::horizontal::pins::BACK_RIGHT_PIN);
 
-	m_motors[i++].set(config::thrusters::veritacal::pins::FRONT_LEFT_PIN);
-	m_motors[i++].set(config::thrusters::veritacal::pins::FRONT_RIGHT_PIN);
-	m_motors[i++].set(config::thrusters::veritacal::pins::BACK_LEFT_PIN);
-	m_motors[i++].set(config::thrusters::veritacal::pins::BACK_RIGHT_PIN);
-	
+	m_motors[i++].set(config::thrusters::horizontal::pins::FRONT_LEFT_PIN, -1);
+	m_motors[i++].set(config::thrusters::horizontal::pins::FRONT_RIGHT_PIN, 1);
+	m_motors[i++].set(config::thrusters::horizontal::pins::BACK_LEFT_PIN, -1);
+	m_motors[i++].set(config::thrusters::horizontal::pins::BACK_RIGHT_PIN, 1);
+
+	m_motors[i++].set(config::thrusters::veritacal::pins::FRONT_LEFT_PIN, 1);
+	m_motors[i++].set(config::thrusters::veritacal::pins::FRONT_RIGHT_PIN, -1);
+	m_motors[i++].set(config::thrusters::veritacal::pins::BACK_LEFT_PIN, -1);
+	m_motors[i++].set(config::thrusters::veritacal::pins::BACK_RIGHT_PIN, 1);	
 }
 
 
@@ -28,6 +27,7 @@ void thruster_subsystem::init()
 	for (int i = 0; i < THRUSTER_SIZE; i++) {
 		m_motors[i].init();
 	}
+
 }
 
 void thruster_subsystem::run(const data_store & store)
